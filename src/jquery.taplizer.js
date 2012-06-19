4d944daf59e0462ff2a46c3309301368cb4a3228
Daniel <daniel@unfold.no>
2012-06-19 14:47:27 +0200

    Initial commit

---
+++ src/jquery.taplizer.js
@@ -0,0 +1,54 @@
+/* jquery.taplizer by Unfold */
+/* Use -webkit-tap-highlight-color: transparent; to remove tap highlight */
+
+window.hasOwnProperty('ontouchstart') && (function() {
+    var moveThreshold = 10;
+
+    var events = {
+        start: 'touchstart',
+        move: 'touchmove',
+        end: 'touchend'
+    };
+
+    $.event.special.click = {
+        add: function(handle) {
+            var originalHandler = handle.handler;
+
+            handle.handler = function(e, triggered) {
+                if(!triggered) return false;
+                originalHandler.call(this, e);
+            };
+
+            $(this).on(events.start, $.proxy(start, this));
+        },
+
+        remove: function() {
+            $(this).off(events.start, $.proxy(start, this));
+        }
+    };
+
+    var start = function(e) {
+        var startX = e.originalEvent.touches[0].screenX;
+        var startY = e.originalEvent.touches[0].screenY;
+
+        var move = function(e) {
+            var movedX = Math.abs(e.originalEvent.touches[0].screenX - startX) > moveThreshold;
+            var movedY = Math.abs(e.originalEvent.touches[0].screenY - startY) > moveThreshold;
+
+            (movedX || movedY) && unbind();
+        };
+
+        var end = function() {
+            unbind();
+            $(this).trigger('click', true);
+        };
+
+        var unbind = $.proxy(function() {
+            $(this).off(events.move, $.proxy(move, this));
+            $(this).off(events.end, $.proxy(end, this));
+        }, this);
+
+        $(this).on(events.move, $.proxy(move, this));
+        $(this).on(events.end, $.proxy(end, this));
+    };
+})();
